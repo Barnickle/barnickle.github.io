69998ba3ae27168199dd84542bd85c774dd1c225
Barnickle <bfradl@hsr.ch>
2014-11-15 23:44:32 +0100

    fvh

---
+++ Eleken_files/animated.js
@@ -0,0 +1,131 @@
+$(document).ready(function() {
+
+    enquire.register("screen and (min-width: 1000px)", function() {
+       
+	    jQuery(document).ready(function() {
+			jQuery('.animated-down-05s').addClass("hidden").viewportChecker({
+			    classToAdd: 'visible animated-05s fadeInDown',
+			    offset: 0
+			   });
+		});
+
+
+		jQuery(document).ready(function() {
+			jQuery('.animated-down-1s').addClass("hidden").viewportChecker({
+			    classToAdd: 'visible animated-1s fadeInDown',
+			    offset: 0
+			   });
+		});
+
+		jQuery(document).ready(function() {
+			jQuery('.animated-down-1-2s').addClass("hidden").viewportChecker({
+			    classToAdd: 'visible animated-1-2s fadeInDown',
+			    offset: 0
+			   });
+		});
+
+		jQuery(document).ready(function() {
+			jQuery('.animated-down-1-5s').addClass("hidden").viewportChecker({
+			    classToAdd: 'visible animated-1-5s fadeInDown',
+			    offset: 0
+			   });
+		});
+
+		jQuery(document).ready(function() {
+			jQuery('.animated-down-2s').addClass("hidden").viewportChecker({
+			    classToAdd: 'visible animated-2s fadeInDown',
+			    offset: 0
+			   });
+		});
+
+		jQuery(document).ready(function() {
+			jQuery('.animated-down-2s-of-100').addClass("hidden").viewportChecker({
+			    classToAdd: 'visible animated-2s fadeInDown',
+			    offset: 100
+			   });
+		});
+
+		jQuery(document).ready(function() {
+			jQuery('.animated-down-3s').addClass("hidden").viewportChecker({
+			    classToAdd: 'visible animated-3s fadeInDownHeader',
+			    offset: 0
+			   });
+		});
+		// jQuery(document).ready(function() {
+		// 	jQuery('.animated-down-3s').addClass("hidden").viewportChecker({
+		// 	    classToAdd: 'visible animated-3s fadeInDown', // Class to add to the elements when they are visible
+		// 	    offset: 0
+		// 	   });
+		// });
+
+		// animate Down end
+
+		jQuery(document).ready(function() {
+			jQuery('.animate-opacity').addClass("hidden").viewportChecker({
+			    classToAdd: 'visible animated-05s fadeIn',
+			    offset: 0
+			   });
+		});
+
+		jQuery(document).ready(function() {
+			jQuery('.animate-right').addClass("hidden").viewportChecker({
+			    classToAdd: 'visible animated-1-2s bounceInRight',
+			    offset: 0
+			   });
+		});
+
+		jQuery(document).ready(function() {
+			jQuery('.animate-right-2').addClass("hidden").viewportChecker({
+			    classToAdd: 'visible animated-1-2s bounceInRight-2',
+			    offset: 0
+			   });
+		});
+
+		jQuery(document).ready(function() {
+			jQuery('.animate-opacity-1s').addClass("hidden").viewportChecker({
+			    classToAdd: 'visible animated-1s fadeIn',
+			    offset: 0
+			   });
+		});
+
+		jQuery(document).ready(function() {
+			jQuery('.animate-opacity-1-5s').addClass("hidden").viewportChecker({
+			    classToAdd: 'visible animated-1-5s fadeIn',
+			    offset: 0
+			   });
+		});
+
+		jQuery(document).ready(function() {
+			jQuery('.animate-opacity-2-8s').addClass("hidden").viewportChecker({
+			    classToAdd: 'visible animated-2-8s fadeIn',
+			    offset: 0
+			   });
+		});
+
+
+		jQuery(document).ready(function() {
+			jQuery('.animate-up').addClass("hidden").viewportChecker({
+			    classToAdd: 'visible animated-3s fadeOutUp',
+			    offset: 0
+			   });
+		});
+
+		jQuery(document).ready(function() {
+			jQuery('.animate-40').addClass("hidden").viewportChecker({
+			    classToAdd: 'visible animated-1-5s fadeInDown-40',
+			    offset: 0
+			   });
+		});
+
+		jQuery(document).ready(function() {
+			jQuery('.animate-60').addClass("hidden").viewportChecker({
+			    classToAdd: 'visible animated-1-5s fadeInDown-60',
+			    offset: 0
+			   });
+		});
+
+    })
+
+}); //note: 
+	// animate Down start
+	
